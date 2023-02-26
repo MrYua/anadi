c4ca54e10dbc6ee467e219e9f13de3caee5c53d9
Mr_GaoYu <Mr_GaoYu@163.com>
2023-02-26 21:54:13 +0800

    dashboard

--- packages/dashboard/app.vue
+++ packages/dashboard/app.vue
@@ -1,5 +1,4 @@
 <template>
   <div>
-    <Palette />
   </div>
 </template>
--- packages/dashboard/components/skeleton.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="skeleton"></div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style lang="less" scoped></style>
