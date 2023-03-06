7fa1a510b92ba3f0581e01fb3a2363dab91d0dde
Mr_GaoYu <Mr_GaoYu@163.com>
2023-03-06 18:19:11 +0800

    anadi

--- packages/cli/bin/anadi.ts
+++ packages/cli/bin/anadi.ts
@@ -3,6 +3,9 @@ import {Command} from "commander";
 
 async function bootstrap() {
     const program = new Command();
+
+    program.version(`varlet-cli`).usage('<command> [options]')
+
 }
 
 bootstrap();
