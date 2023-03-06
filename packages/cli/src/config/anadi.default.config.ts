cd6a647c83c5d9b4a3cef98eb5bbd704108d2c3d
Mr_GaoYu <Mr_GaoYu@163.com>
2023-03-06 14:54:46 +0800

    初始化

--- packages/cli/src/config/anadi.default.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "./anadi.config.js";
-
-export default defineConfig({
-  name: "Anadi",
-  namespace: "anadi",
-  host: "localhost",
-  port: 8080,
-  title: "ANADI",
-});
--- packages/cli/src/utilties/fs.ts
+++
@@ -1,8 +0,0 @@
-import fse from "fs-extra";
-import { UI_PACKAGE_JSON } from "../settings/constant.js";
-
-const { readJSONSync } = fse;
-
-export function getVersion() {
-  return readJSONSync(UI_PACKAGE_JSON).version;
-}
