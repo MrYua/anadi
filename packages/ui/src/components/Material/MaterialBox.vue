a38a4406bc295d0b45d4b5cd05b27c1af0ef11fa
Mr_GaoYu <Mr_GaoYu@163.com>
2023-03-13 18:58:41 +0800

    初始化

---
+++ packages/ui/src/components/Material/MaterialBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="material-box">
+    <div class="material-box__header">
+      <slot name="header"></slot>
+    </div>
+
+    <div class="material-box__content">
+      <slot></slot>
+    </div>
+
+    <div class="material-box__footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
+import jsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
-  plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "#": resolve(__dirname, "types"),
+    },
+  },
+  plugins: [vue(), jsx()],
+});
