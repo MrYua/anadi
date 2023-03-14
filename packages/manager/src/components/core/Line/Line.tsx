accce40fe7bb9e8f1d3546a098eb9e11e0e9d2c6
Mr_GaoYu <Mr_GaoYu@163.com>
2023-03-14 21:28:40 +0800

    chushihua

---
+++ packages/manager/src/components/core/Line/Line.tsx
@@ -0,0 +1,8 @@
+import { defineComponent } from "vue";
+
+export const Line = defineComponent({
+  name: "Line",
+  setup() {
+    return () => <div></div>;
+  },
+});
---
+++ packages/manager/src/components/core/Line/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from "@/utilties/withInstall";
+import { Line as _Line } from "./Line";
+
+export const Line = withInstall(_Line);
+
+export default Line;
